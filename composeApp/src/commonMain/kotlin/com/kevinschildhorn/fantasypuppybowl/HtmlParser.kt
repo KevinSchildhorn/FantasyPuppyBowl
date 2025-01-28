91bee3a8a0906d39d6f923bd2874ad73f3409ccd
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2025-01-27 23:17:54 -0500

    fixing parsing to get all dogs

--- composeApp/src/commonMain/kotlin/com/kevinschildhorn/fantasypuppybowl/HtmlParser.kt
+++ composeApp/src/commonMain/kotlin/com/kevinschildhorn/fantasypuppybowl/HtmlParser.kt
@@ -6,11 +6,12 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonIgnoreUnknownKeys
 import org.jsoup.Jsoup
+import org.jsoup.nodes.TextNode
 import java.net.HttpURLConnection
 import java.net.URL
 
 class HTMLParser {
-    suspend fun getDogInfo(urlString: String):List<ListItem> {
+    suspend fun getDogInfo(urlString: String): List<ListItem> {
         val url = URL(urlString)
         val urlConnection = url.openConnection() as HttpURLConnection
 
@@ -22,7 +23,47 @@ class HTMLParser {
         }
     }
 
-    private fun parseHtml(html: String):List<ListItem> {
+    private fun parseHtml(html: String): List<ListItem> {
+        val ksoupHtmlParser = KsoupHtmlParser()
+        ksoupHtmlParser.write(html)
+        val doc = Jsoup.parse(html)
+        val names = doc.body().allElements.filter { element ->
+            element.hasClass("o-PhotoGalleryPromo__m-AssetData")
+        }.map {
+            val nameElement =
+                it.allElements.find { element -> element.hasClass("o-PhotoGalleryPromo__a-HeadlineText") }
+            val descriptionElement =
+                it.allElements.find { element -> element.hasClass("o-PhotoGalleryPromo__a-Description") }
+
+            val description = (descriptionElement?.childNodes()?.first()?.childNodes()
+                ?.first() as? TextNode)?.text() // TODO
+            (nameElement?.childNodes()?.first() as? TextNode)?.text() to description
+
+        }
+        val imagesMobile = doc.body().allElements.filter { element ->
+            element.hasClass("m-MediaBlock__a-Image")
+        }.map {
+            "https:" + it.attr("src")
+        }
+        val imagesDesktop = doc.body().allElements.filter { element -> // TODO: For Desktop Sites
+            element.hasClass("rsImg")
+        }.map {
+            val attr = it.attr("src").ifEmpty { null } ?: it.attr("href")
+            "https:" + attr
+        }
+        val finalImages = if(imagesMobile.size > imagesDesktop.size) imagesMobile else imagesDesktop
+
+        val result = names.mapIndexed { index, pair ->
+            ListItem(
+                name = pair.first ?: "",
+                description = pair.second ?: "",
+                image = finalImages[index] ?: "",
+            )
+        }
+        return result
+    }
+
+    private fun parseHtml_old(html: String): List<ListItem> {
         val ksoupHtmlParser = KsoupHtmlParser()
         ksoupHtmlParser.write(html)
         val doc = Jsoup.parse(html)
@@ -35,7 +76,7 @@ class HTMLParser {
 
         val dogs = Json.decodeFromString<List<ScriptContents>>(html)
         val dogList = dogs.filterNot { it.mainEntity == null }.first()
-        val items:List<ListItem>? = dogList.mainEntity?.itemListElement?.map {
+        val items: List<ListItem>? = dogList.mainEntity?.itemListElement?.map {
             it.item
         }
         print(items)
